2f1bdcb4404a1732522e48b11bb44d7222215463
Mateusz Strózik <strozikmateusz@gmail.com>
2026-05-13 20:34:22 +0200

    fix: move consumer method to private for better readability

--- src/modules/analytics/analytics.consumer.ts
+++ src/modules/analytics/analytics.consumer.ts
@@ -7,27 +7,23 @@ import {
 } from './analytics.interface.js';
 
 export class AnalyticsConsumer implements IAnalyticsConsumer {
-  private readonly onGetAnalytic: (
-    shortCode: string,
-    ipAddress: string,
-  ) => void;
   private readonly batch: AnalyticDTO[] = [];
 
   constructor(
     private readonly analyticsService: IAnalyticsService,
     private readonly eventEmitter: EventEmitter,
     private readonly batchSize = 100,
-  ) {
-    this.onGetAnalytic = (shortCode: string, ipAddress: string) => {
-      this.batch.push({ shortCode, ipAddress, createdAt: new Date() });
-
-      if (this.batch.length >= this.batchSize) {
-        void this.analyticsService.saveAnalytic(this.batch.splice(0));
-      }
-    };
-  }
+  ) {}
 
   registerListeners(): void {
     this.eventEmitter.on(URL_ANALYTIC_EVENT, this.onGetAnalytic);
   }
+
+  private onGetAnalytic = (shortCode: string, ipAddress: string) => {
+    this.batch.push({ shortCode, ipAddress, createdAt: new Date() });
+
+    if (this.batch.length >= this.batchSize) {
+      void this.analyticsService.saveAnalytic(this.batch.splice(0));
+    }
+  };
 }
